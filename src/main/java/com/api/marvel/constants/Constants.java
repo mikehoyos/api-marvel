2cd2bd812036114b2dd00a31fe19d52b591cd9be
Mike Hoyos <mikehoyos7@gmail.com>
2025-02-03 18:33:45 -0600

    Se realiza commit para documentar codigo y se modifica el git ingone

--- src/main/java/com/api/marvel/constants/Constants.java
+++ src/main/java/com/api/marvel/constants/Constants.java
@@ -3,13 +3,32 @@ package com.api.marvel.constants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+/**
+ * Clase con constantes necesarias para el api.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constants {
 
+    /**
+     * Constante con la base del paquete.
+     */
     public static final String BASE_PACKAGE = "com.api.marvel";
-    public static final String POST_VAL_DEST_ACCOUNT = "${api.uri.mapping.validate.dest.account}";
-
+    /**
+     * Constante con el nombre del feing client.
+     */
     public static final String FEIGN_NAME_MARVEL = "${constants.api.feign.name.marvel}";
+    /**
+     * Contante con la url del feign client.
+     */
     public static final String FEIGN_URL_MARVEL  = "${constants.api.feign.url.marvel}";
+    /**
+     * Constante con el path del feign client.
+     */
     public static final String FEIGN_PATH_MARVEL ="${constants.api.feign.path.marvel}";
+
+    /**
+     * Contante con el valor del reques maping de metddo
+     *
+     */
+    public static final String RQ_MAP = "characters";
 }
